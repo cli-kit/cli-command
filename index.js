b58aecf7d4c1fea3a17f42ec3704ff475b4b9373
cyberfunk <hello@cyberfunk.org>
2014-01-30 05:28:56 +0800

    Run subcommand executables.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+var path = require('path'), dirname = path.dirname, basename = path.basename;
+var spawn = require('child_process').spawn;
 var cli = require('cli-define');
 var parser = require('cli-argparse');
+
 var actions = {
   help: require('./lib/help'),
   version: require('./lib/version')
@@ -18,7 +21,6 @@ function configuration() {
       config.options = config.options.concat(arg.names);
     }
   }
-  //console.dir(config);
   return config;
 }
 
@@ -43,11 +45,48 @@ function builtins() {
   }
 }
 
+function execute(argv, cmd, args) {
+  var dir = dirname(argv[1]);
+  var bin = basename(argv[1]) + '-' + cmd;
+  var local = path.join(dir, bin);
+  //console.log('%s %s %s %j', dir, bin, local, args)
+  var ps = spawn(local, args, { stdio: 'inherit'});
+  ps.on('error', function(err){
+    if (err.code == "ENOENT") {
+      console.error('%s(1) does not exist, try --help', bin);
+    } else if (err.code == "EACCES") {
+      console.error('%s(1) not executable. try chmod or run with root', bin);
+    }
+  });
+  ps.on('close', function (code, signal) {
+    // NOTE: workaround for https://github.com/joyent/node/issues/3222
+    // NOTE: assume child process exited gracefully on SIGINT
+    if(signal == 'SIGINT') {
+      process.exit(0);
+    }
+    process.exit(code);
+  });
+}
+
+function command() {
+  var z, i, raw = this._args.raw.slice(0), action, cmd, arg;
+  for(i = 0;i < raw.length;i++) {
+    cmd = raw[i]; arg = this._commands[cmd];
+    if(arg && !arg.action) {
+      raw.splice(i, 1);
+      return execute.call(this, process.argv, cmd, raw);
+    }else if(arg && arg.action) {
+      // TODO: invoke command action function
+    }
+  }
+}
+
 function parse(args) {
   var config = configuration.call(this);
   this._args = parser(args, config);
   merge.call(this);
   builtins.call(this);
+  command.call(this);
 }
 
 module.exports = function(package, name, description) {
