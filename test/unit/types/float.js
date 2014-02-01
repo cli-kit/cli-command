a57293a165daeb05337dcb0a19eaeb6491d6cd0f
cyberfunk <hello@cyberfunk.org>
2014-02-01 15:32:04 +0800

    Add suport for float and number types.

---
+++ test/unit/types/float.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var expect = require('chai').expect;
+var pkg = path.normalize(path.join(__dirname, '..', '..', '..', 'package.json'));
+var cli = require('../../..')(pkg);
+var types = require('../../..').types;
+
+describe('cli-command:', function() {
+  var pi = 3.14159265359;
+  var golden = 1.61803398875;
+  it('should coerce single value to float', function(done) {
+    var args = ['-f', '' + pi];
+    cli
+      .option('-f, --float <n>', 'a float argument', types.float)
+    cli.parse(args);
+    expect(cli.float).to.eql(pi);
+    done();
+  });
+  it('should coerce multiple values to array of floats', function(done) {
+    var args = ['-f', '' + pi, '--float=' + golden];
+    cli
+      .option('-f, --float <n...>', 'a float argument', types.float)
+    cli.parse(args);
+    expect(cli.float).to.eql([pi, golden]);
+    done();
+  });
+  it('should error on invalid float', function(done) {
+    var args = ['-f', 'xyz'];
+    cli
+      .error(function(code, codes, message, parameters, data) {
+        expect(cli).to.eql(this);
+        expect(code).to.eql(codes.ETYPE);
+        //parameters.unshift(message);
+        //console.error.apply(null, parameters);
+      })
+      .option('-f, --float <n>', 'a float argument', types.float)
+    cli.parse(args);
+    done();
+  });
+  it('should error on invalid float in array', function(done) {
+    var args = ['-f', '' + pi, '--float=' + golden, '--float', 'zyx'];
+    cli
+      .error(function(code, codes, message, parameters, data) {
+        expect(cli).to.eql(this);
+        expect(code).to.eql(codes.ETYPE);
+        //parameters.unshift(message);
+        //console.error.apply(null, parameters);
+      })
+      .option('-f, --float <n...>', 'a float argument', types.float)
+    cli.parse(args);
+    done();
+  });
+})
---
+++ test/unit/types/number.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var expect = require('chai').expect;
+var pkg = path.normalize(path.join(__dirname, '..', '..', '..', 'package.json'));
+var cli = require('../../..')(pkg);
+var types = require('../../..').types;
+
+describe('cli-command:', function() {
+  var pi = 3.14159265359;
+  var golden = 1.61803398875;
+  var integer = 128;
+  it('should coerce single value to number', function(done) {
+    var args = ['-n', '' + pi];
+    cli
+      .option('-n, --number <n>', 'a number argument', types.number)
+    cli.parse(args);
+    expect(cli.number).to.eql(pi);
+    done();
+  });
+  it('should coerce multiple values to array of numbers', function(done) {
+    var args = ['-n', '' + pi, '--number=' + golden, '--number', '' + integer];
+    cli
+      .option('-n, --number <n...>', 'a number argument', types.number)
+    cli.parse(args);
+    expect(cli.number).to.eql([pi, golden, integer]);
+    done();
+  });
+  it('should error on invalid number', function(done) {
+    var args = ['-n', 'xyz'];
+    cli
+      .error(function(code, codes, message, parameters, data) {
+        expect(cli).to.eql(this);
+        expect(code).to.eql(codes.ETYPE);
+        //parameters.unshift(message);
+        //console.error.apply(null, parameters);
+      })
+      .option('-n, --number <n>', 'a number argument', types.number)
+    cli.parse(args);
+    done();
+  });
+  it('should error on invalid number in array', function(done) {
+    var args = ['-n', '' + pi, '--number=' + golden, '--number', 'zyx'];
+    cli
+      .error(function(code, codes, message, parameters, data) {
+        expect(cli).to.eql(this);
+        expect(code).to.eql(codes.ETYPE);
+        //parameters.unshift(message);
+        //console.error.apply(null, parameters);
+      })
+      .option('-n, --number <n...>', 'a number argument', types.number)
+    cli.parse(args);
+    done();
+  });
+})
