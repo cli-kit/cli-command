b824aa59c70f56248831d357bf842d2162adea8e
cyberfunk <hello@cyberfunk.org>
2014-02-02 23:48:13 +0800

    Test setting configuration.
    100% code coverage.

---
+++ test/unit/configuration.js
@@ -0,0 +1,16 @@
+var path = require('path');
+var expect = require('chai').expect;
+var pkg = path.normalize(path.join(__dirname, '..', '..', 'package.json'));
+
+describe('cli-command:', function() {
+  it('should set configuration (exit)', function(done) {
+    var cli = require('../..')(pkg, 'configuration');
+    var args = [];
+    cli
+      .configuration({exit: false})
+      .parse(args);
+    var config = cli.configuration();
+    expect(config.exit).to.eql(false);
+    done();
+  });
+})
