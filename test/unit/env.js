3a4f75bffabd316904eed4d055b89da56de7f8df
cyberfunk <hello@cyberfunk.org>
2014-02-07 10:17:41 +0800

    Improve env middleware assignment logic.

--- test/unit/env.js
+++ test/unit/env.js
@@ -66,6 +66,7 @@ describe('cli-command:', function() {
         .option('-u --url [url]', 'a url')
         .parse(args);
       expect(cli.url).to.eql(value);
+      expect(cli._arguments.url.value()).to.eql(value);
       expect(cli.rc).to.eql(undefined);
       done();
     }
