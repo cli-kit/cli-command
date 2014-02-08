dafe35996ed4d25f1fbd23f000870618f9217575
cyberfunk <hello@cyberfunk.org>
2014-02-08 20:36:52 +0800

    Assert on -f file type tests.

--- test/unit/types/file.js
+++ test/unit/types/file.js
@@ -4,15 +4,32 @@ var pkg = path.normalize(
   path.join(__dirname, '..', '..', '..', 'package.json'));
 var types = require('../../..').types;
 
+var files = {
+  regular: pkg,
+  missing: 'this-file-really-does-not-want-to-be-found.txt'
+}
+
 describe('cli-command:', function() {
   it('should be a file type', function(done) {
-    var cli = require('../../..')(pkg);
-    var value = '/etc/passwd';
+    var cli = require('../../..')(pkg, 'mock-file-type');
+    var value = files.regular;
     var args = ['-f', value];
     cli
       .option('-f, --file <file>', 'a file argument', types.file('f'))
     cli.parse(args);
     expect(cli.file).to.eql(value);
     done();
+  });
+  it('should throw argument type error (missing)', function(done) {
+    var cli = require('../../..')(pkg, 'mock-file-type');
+    var value = files.missing;
+    var args = ['-f', value];
+    cli
+      .option('-f, --file <file>', 'a file argument', types.file('f'))
+      .once('error', function(e) {
+        expect(e.code).to.eql(this.errors.EFILE_TYPE_F.code);
+        done();
+      })
+    cli.parse(args);
   });
 })
