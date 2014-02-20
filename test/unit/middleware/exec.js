3c89e567c6e87607453371b5f6af8b4f39db56d5
cyberfunk <hello@cyberfunk.org>
2014-02-20 20:43:21 +0800

    Assert on exec middleware errors.
    100% code coverage, closes #51.

---
+++ test/unit/middleware/exec.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var expect = require('chai').expect;
+var error = require('../../../lib/middleware/exec').error;
+describe('cli-command:', function() {
+  it('should raise error (EACCES)', function(done) {
+    var cli = require('../../..');
+    cli = cli(null, 'middleware/exec');
+    cli.on('error', function(e) {
+      done();
+    })
+    error(cli, {code: 'EACCES'});
+  });
+  it('should ignore unknown error (EUNKNOWN)', function(done) {
+    var cli = require('../../..');
+    cli = cli(null, 'middleware/exec');
+    error(cli, {code: 'EUNKNOWN'});
+    done();
+  });
+});
