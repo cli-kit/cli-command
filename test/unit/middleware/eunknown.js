46ebc6c90ef069e417f070f97a52e6f329dce518
cyberfunk <hello@cyberfunk.org>
2014-02-20 20:03:31 +0800

    Assert on eunknown middleware.

---
+++ test/unit/middleware/eunknown.js
@@ -0,0 +1,15 @@
+var path = require('path');
+var expect = require('chai').expect;
+describe('cli-command:', function() {
+  it('should handle eunknown middleware without unparsed', function(done) {
+    var cli = require('../../..');
+    var middleware = cli.middleware;
+    var conf = {};
+    cli = cli()
+      .use(middleware.parser)
+      .use(middleware.eunknown)
+      .parse([]);
+    expect(cli._middleware.length).to.eql(2);
+    done();
+  });
+});
