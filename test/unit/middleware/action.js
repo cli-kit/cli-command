507ceac1f0f0921307c154f8df0165aaedadc5cc
cyberfunk <hello@cyberfunk.org>
2014-02-20 18:25:30 +0800

    Assert on action middleware code path.

---
+++ test/unit/middleware/action.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var expect = require('chai').expect;
+describe('cli-command:', function() {
+  it('should handle action middleware with no parser', function(done) {
+    var cli = require('../../..');
+    var middleware = cli.middleware;
+    var conf = {};
+    cli = cli()
+      .use(middleware.action)
+      .parse([]);
+    expect(cli._middleware.length).to.eql(1);
+    done();
+  });
+});
