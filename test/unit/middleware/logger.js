af2fa137d949b6e656fcdecd39431377b0acc2d0
cyberfunk <hello@cyberfunk.org>
2014-02-21 22:23:08 +0800

    Assert on log file stream error.

--- test/unit/middleware/logger.js
+++ test/unit/middleware/logger.js
@@ -2,9 +2,16 @@ var path = require('path');
 var expect = require('chai').expect;
 var ttycolor = require('ttycolor');
 var logger = require('cli-logger');
+var keys = logger.keys;
 
 var file = path.normalize(
   path.join(__dirname, '..', '..', '..', 'log', 'logger-test.log'));
+
+var redirect = path.normalize(
+  path.join(__dirname, '..', '..', '..', 'log', 'logger-redirect.log'));
+
+
+var invalid = path.join('/', 'bin', 'invalid-logger.log');
 
 var conf = {
   streams: [
@@ -66,6 +73,37 @@ describe('cli-command:', function() {
         done();
       })
       .parse([]);
+  });
+  it('should use logger middleware (--log-file)', function(done) {
+    var cli = require('../../..');
+    var middleware = cli.middleware;
+    cli = cli()
+      .use(middleware.logger, null, {file: {}})
+      .on('complete', function(req) {
+        expect(this.logFile).to.eql(redirect);
+        expect(this._options.logFile).to.be.an('object');
+        expect(this._options.logLevel).to.eql(undefined);
+        var log = this.log;
+        keys.forEach(function(key) {
+          log[key]('mock %s message', key);
+        })
+        done();
+      })
+      .parse(['--log-file=' + redirect]);
+  });
+  it('should error on log file (--log-file)', function(done) {
+    var cli = require('../../..');
+    var middleware = cli.middleware;
+    cli = cli()
+      .use(middleware.logger, null, {file: {}})
+      .on('error', function(e) {
+        function fn() {
+          throw e;
+        }
+        expect(fn).throws(Error);
+        done();
+      })
+      .parse(['--log-file=' + invalid]);
   });
   it('should print error via logger', function(done) {
     var cli = require('../../..');
