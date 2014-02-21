ce69afc67742e5f9b68b0be8d7788b98566c4e1c
cyberfunk <hello@cyberfunk.org>
2014-02-21 20:04:22 +0800

    Assert on logger middleware.
    Closes #44.

--- test/unit/middleware/logger.js
+++ test/unit/middleware/logger.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var expect = require('chai').expect;
+var ttycolor = require('ttycolor');
 describe('cli-command:', function() {
   it('should use logger middleware', function(done) {
     var cli = require('../../..');
@@ -13,14 +14,28 @@ describe('cli-command:', function() {
       })
       .parse([]);
   });
+  it('should use logger middleware with configuration', function(done) {
+    var cli = require('../../..');
+    var middleware = cli.middleware;
+    cli = cli()
+      .use(middleware.logger, {console: false})
+      .on('complete', function(req) {
+        expect(this.log).to.be.an('object');
+        expect(this.log).to.be.instanceof(require('cli-logger').Logger);
+        done();
+      })
+      .parse([]);
+  });
   it('should print error via logger', function(done) {
     var cli = require('../../..');
     var middleware = cli.middleware;
     cli = cli(null, 'mock-logger-error')
       .configure({exit: false})
+      .use(middleware.color)
       .use(middleware.logger)
       .option('--required <value>', 'required option')
       .on('complete', function(req) {
+        if(typeof ttycolor.revert === 'function') ttycolor.revert();
         done();
       })
       .parse([]);
