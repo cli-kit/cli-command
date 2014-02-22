d51e254b7bc6f237f1d2bfb0aa978b17c0d064b1
cyberfunk <hello@cyberfunk.org>
2014-02-22 20:17:21 +0800

    Update enum tests.

--- test/unit/types/enum.js
+++ test/unit/types/enum.js
@@ -54,4 +54,101 @@ describe('cli-command:', function() {
         'an enum argument', types.enum(list))
     cli.parse(args);
   });
+  it('should allow primitive in enum list (null)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = 'null';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(null);
+    done();
+  });
+  it('should allow primitive in enum list (false)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = 'false';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(false);
+    done();
+  });
+  it('should allow primitive in enum list (true)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = 'true';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(true);
+    done();
+  });
+  it('should allow primitive in enum list (1)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = '1';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(1);
+    done();
+  });
+  it('should allow primitive in enum list (-1)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = '-1';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(-1);
+    done();
+  });
+  it('should allow primitive in enum list (undefined)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined];
+    var value = 'undefined';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql(undefined);
+    done();
+  });
+  it('should allow primitive in enum list (string)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined, 'string'];
+    var value = 'string';
+    var args = ['-e', value];
+    cli
+      .option('-e, --enum <value>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql('string');
+    done();
+  });
+  it('should allow primitives in enum list (repeatable)', function(done) {
+    var cli = require('../../..')(pkg);
+    var list = [null, false, true, 1, -1, undefined, 'string'];
+    var args = [
+      '-e=null', '-e=false', '-e=true', '-e=1', '-e=-1', '-e', '-1',
+      '-e=undefined', '-e=string'];
+    cli
+      .option('-e, --enum <value...>',
+        'an enum argument', types.enum(list, true))
+    cli.parse(args);
+    expect(cli.enum).to.eql([null, false, true, 1, -1, -1, undefined, 'string']);
+    done();
+  });
 })
